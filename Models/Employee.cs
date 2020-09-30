e822b3513d422eeca93a9a8766dc44b658072d3b
prameshKc <prameshcasey@gmail.com>
2020-10-01 05:04:26 +0545

    employeee model

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -7,5 +7,7 @@ namespace sample.Models
         public string lastName  { get; set; }
         public string email  { get; set; }
         public string address  { get; set; }
+        public string username  { get; set; }
+        public string password  { get; set; }
     }
 }
